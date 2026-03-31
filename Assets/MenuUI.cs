d81b246e97fe4e963b345deb6b228c4fd71903a9
wcy03142215 <2949184831@qq.com>
2026-03-31 18:44:46 +0800

    增加武器特效

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,158 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject mapSelectPanel;
+    public GameObject settingsPanel;
+    public GameObject pausePanel;
+
+    public Slider volumeSlider;
+    public Toggle fullscreenToggle;
+    public TMP_Dropdown resolutionDropdown;
+
+    public TMP_Text descriptionTitle;
+    public TMP_Text descriptionText;
+
+    void Start()
+    {
+        if (volumeSlider != null)
+            volumeSlider.value = AudioListener.volume;
+
+        if (fullscreenToggle != null)
+            fullscreenToggle.isOn = Screen.fullScreen;
+
+        ShowDefaultMapInfo();
+    }
+
+    public void OpenMapSelect()
+    {
+        if (mainPanel != null) mainPanel.SetActive(false);
+        if (mapSelectPanel != null) mapSelectPanel.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        if (mainPanel != null) mainPanel.SetActive(false);
+        if (settingsPanel != null) settingsPanel.SetActive(true);
+    }
+
+    public void BackToMainFromMap()
+    {
+        if (mapSelectPanel != null) mapSelectPanel.SetActive(false);
+        if (mainPanel != null) mainPanel.SetActive(true);
+    }
+
+    public void BackToMainFromSettings()
+    {
+        if (settingsPanel != null) settingsPanel.SetActive(false);
+        if (mainPanel != null) mainPanel.SetActive(true);
+    }
+
+    public void OpenPause()
+    {
+        if (pausePanel != null) pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ClosePause()
+    {
+        if (pausePanel != null) pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenuScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("UITest");
+    }
+
+    public void LoadMap1()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void LoadMap2()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadMap3()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    //地图选择文本相关代码
+    public void ShowMap1Info()
+    {
+        if (descriptionTitle != null)
+            descriptionTitle.text = "MAP　ONE";
+
+        if (descriptionText != null)
+            descriptionText.text = "abc";
+    }
+
+    public void ShowMap2Info()
+    {
+        if (descriptionTitle != null)
+            descriptionTitle.text = "MAP TWO";
+
+        if (descriptionText != null)
+            descriptionText.text = "def";
+    }
+
+    public void ShowMap3Info()
+    {
+        if (descriptionTitle != null)
+            descriptionTitle.text = "MAP THRIE";
+
+        if (descriptionText != null)
+            descriptionText.text = "hij";
+    }
+    public void ShowDefaultMapInfo()
+    {
+        if (descriptionTitle != null)
+            descriptionTitle.text = "MAP SELECT";
+
+        if (descriptionText != null)
+            descriptionText.text = "PLEASE SELECT A MAP";
+    }
+    //文本代码结束
+
+    public void SetResolution(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
